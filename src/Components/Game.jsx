64f980df708711165370c7019956c2773e97ea6f
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-11-03 16:01:45 -0500

    Set up chessboard and where to add game mechanics

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,12 +1,30 @@
+import { useState, useMemo, useCallback, useEffect } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import CustomDialog from "./CustomDialog";
 
+function Game({ players, room, orientation, cleanup }) {
+  const chess = useMemo(() => new Chess(), []);
+  const [fen, setFen] = useState(chess.fen());
+  const [over, setOver] = useState("");
 
-function Game() {
-
+  function onDrop() {}
+  
   return (
     <>
-      <h1>Game Page</h1>
+      <div className="board">
+        <Chessboard position={fen} onPieceDrop={onDrop} />  {/**  <- 4 */}
+      </div>
+      <CustomDialog
+        open={Boolean(over)}
+        title={over}
+        contentText={over}
+        handleContinue={() => {
+          setOver("");
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default Game
+export default Game;
