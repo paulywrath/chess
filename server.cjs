92fa5faa42c5a439d5d3c0c6c0a96a02d4c53850
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-11-08 15:15:28 -0500

    Handle HTTP requests and WebSocket connections

---
+++ server.cjs
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Server } = require("socket.io");
+const { v4: uuidV4 } = require('uuid');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8080 
+
+const io = new Server(server, {
+  cors: '*',
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id, 'connected');
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
