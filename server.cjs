a19a465460a1170c13f84bbd13371193b784c1bd
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-11-20 21:39:51 -0500

    Send info to relevant users on attempt to join room
    If joiner attempts to join a room that isn't open to them, send them an
    error message.
    
    If it is, add them to that room and send room and player data to both
    players.

--- server.cjs
+++ server.cjs
@@ -34,6 +34,50 @@ io.on('connection', (socket) => {
 
     callback(roomId);
   });
+
+  socket.on('joinRoom', async (args, callback) => {
+    const room = rooms.get(args.roomId);
+    let error, message;
+  
+    if (!room) {
+      error = true;
+      message = 'room does not exist';
+    } else if (room.length <= 0) {
+      error = true;
+      message = 'room is empty';
+    } else if (room.length >= 2) {
+      error = true;
+      message = 'room is full';
+    }
+
+    if (error) {
+
+      if (callback) {
+        callback({
+          error,
+          message
+        });
+      }
+
+      return;
+    }
+
+    await socket.join(args.roomId);
+
+    const roomUpdate = {
+      ...room,
+      players: [
+        ...room.players,
+        { id: socket.id, username: socket.data?.username },
+      ],
+    };
+
+    rooms.set(args.roomId, roomUpdate);
+
+    callback(roomUpdate);
+
+    socket.to(args.roomId).emit('opponentJoined', roomUpdate);
+  });
 });
 
 server.listen(port, () => {
